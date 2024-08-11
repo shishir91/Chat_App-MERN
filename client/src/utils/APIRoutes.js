a514a64dfc6323887c34f5f409bbe0a5c5974db1
Shishir Shrestha <82019437+shishir91@users.noreply.github.com>
2024-08-11 21:56:47 +0545

    Update APIRoutes.js for deployment

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-mern-server-eta.vercel.app/";
+export const host = "https://chat-app-mern-server-nine.vercel.app/";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
